0143e9d43f15867ef8f53669db6403208dab87bf
Magno Reis <magnoreis.adm@outlook.com>
2017-03-07 19:29:01 -0300

    Página principal construída e operando.

--- index.php
+++ index.php
@@ -1,43 +1,7 @@
 <?php require_once('config.php'); ?>
 
 <?php include(HEADER_TEMPLATE); ?>
-
-
-  <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#"><?php echo $sitename; ?></a>
-
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="<?php BASEURL; ?>">Início <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="/about">Administração</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Blog</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle disabled" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sobre</a>
-          <div class="dropdown-menu" aria-labelledby="dropdown01">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0" action="validacao.php" method="post">
-        <input class="col-md-3 form-control mr-sm-2" type="text" name="usuario" placeholder="Usuário">
-        <input class="form-control mr-sm-2" type="text" name="senha" placeholder="Senha">
-        <button class="btn btn-primary" type="submit">Entrar</button>
-      </form>
-      <button type="button" class="btn btn-success" href="<?php echo BASEURL; ?>/registrar.php">Criar Conta</button>
-    </div>
-  </nav>
-
+<?php include(NAVBAR_TEMPLATE); ?>
   <!-- Main jumbotron for a primary marketing message or call to action -->
   <div class="jumbotron">
     <div class="container-fluid">
